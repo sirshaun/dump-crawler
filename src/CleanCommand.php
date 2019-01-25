d2b2e6e68cc9af1b07f9802959c9f02b32f104c9
Shaun Mhonde <mhondeshaun@gmail.com>
2019-01-25 12:22:25 +1100

    code refactor

--- src/CleanCommand.php
+++ src/CleanCommand.php
@@ -3,12 +3,9 @@
 namespace DumpCrawler;
 
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Helper\ProgressBar;
-use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Finder\Finder;
 
 class CleanCommand extends Command
 {
@@ -21,72 +18,9 @@ class CleanCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        // Regular expression pattern to find dd's
-        $reg = '/^[^(\\\\|#)](\t|\s)*dd\(.*\);$/m';
         $project = $input->getArgument('project');
-
-        $finder = new Finder;
-        ProgressBar::setFormatDefinition('minimal_nomax', '%elapsed:6s%/%estimated:-6s% %memory:6s%');
-        $searchProgressBar = new ProgressBar($output);
-        $searchProgressBar->setFormat('minimal_nomax');
-
-        $output->writeln('<comment>Searching through project...</comment>');
-        $files = null;
-        $getFiles = function ($finder, $project, $reg) {
-            return $finder->files()
-                ->in($project)
-                ->notPath('vendor', 'node_modules') // ignore vendor & node_modules folders
-                ->contains($reg);
-        };
-        // while (!isset($files)) {
-        while ($files = $getFiles($finder, $project, $reg)) {
-            // $files = $getFiles($finder, $project, $reg);
-            $searchProgressBar->advance();
-        }
-        $searchProgressBar->finish();
-
-        $filesProgressBar = new ProgressBar($output, count($files));
-        $filesProgressBar->setFormat('normal');
-
-        $references = [];
-        $filesProgressBar->start();
-        foreach ($files as $file) {
-            $name = basename($file->getRealPath());
-            $contents = $file->getContents();
-            $referenceLines = [];
-
-            // Find the exact lines of the matches ...
-            preg_match_all($reg, $contents, $matches, PREG_OFFSET_CAPTURE);
-            foreach ($matches[0] as $arr) {
-                if (!empty(trim($arr[0]))) {
-                    $line = 1 + substr_count($contents, "\n", 0, $arr[1]);
-                    $referenceLines[] = ['line' => $line, 'content' => trim($arr[0])];
-                }
-            }
-
-            // Note the refernces ...
-            $references[$name] = $referenceLines;
-            $filesProgressBar->advance();
-        }
-        $filesProgressBar->finish();
-        echo PHP_EOL;
-        echo PHP_EOL;
-
-        // Render the output ...
-        if (!empty($references)) {
-            $output->writeln("<error>Found trailing dd's!</error>");
-
-            foreach ($references as $key => $arr) {
-                $table = new Table($output);
-                $output->writeln("<--------------------------------------------------------------------");
-                $output->writeln('<comment>'.$key.'</comment>');
-                $table->setHeaders(['Line', 'Content'])
-                    ->setRows($arr)
-                    ->render();
-                $output->writeln("-------------------------------------------------------------------->");
-            }
-        } else {
-            $output->writeln("<info>No trailing dd's found!</info>");
-        }
+        $output->writeln('<comment>Starting search in project: '.basename($project).' ...</comment>');
+        $fileSearch = new FileSearch($project, $output);
+        $fileSearch->run();
     }
 }
---
+++ src/FileSearch.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace DumpCrawler;
+
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Finder\Finder;
+
+class FileSearch
+{
+    protected $output;
+    protected $project;
+    protected $counter = PHP_INT_MAX;
+
+    public function __construct(string $project, OutputInterface $output)
+    {
+        $this->output = $output;
+        $this->project = $project;
+        $this->reg = '/^[^(\\\\|#)](\t|\s)*dd\(.*\);$/m';
+    }
+
+    public function run()
+    {
+        $references = [];
+
+        // Search through files using finder ...
+        $files = $this->findFiles();
+
+        // Build references from files that have been returned ...
+        $this->buildReferences($files, $references);
+
+        // Output references ...
+        $this->outputReferences($references);
+    }
+
+    protected function buildReferences($files, array &$references)
+    {
+        if (count($files) < 1) {
+            return;
+        }
+
+        $progressBar = new ProgressBar($this->output, count($files));
+        $progressBar->start();
+
+        foreach ($files as $file) {
+            $name = basename($file->getRealPath());
+            $contents = $file->getContents();
+            $referenceLines = [];
+
+            // Find the exact lines of the matches ...
+            $this->findInContent($contents, $referenceLines);
+
+            // Note the refernces ...
+            $references[$name] = $referenceLines;
+
+            $progressBar->advance();
+        }
+
+        $progressBar->finish();
+    }
+
+    protected function findInContent(string $contents, array &$referenceLines)
+    {
+        preg_match_all($this->reg, $contents, $matches, PREG_OFFSET_CAPTURE);
+        foreach ($matches[0] as $arr) {
+            if (!empty(trim($arr[0]))) {
+                $line = 1 + substr_count($contents, "\n", 0, $arr[1]);
+                $referenceLines[] = ['line' => $line, 'content' => trim($arr[0])];
+            }
+        }
+    }
+
+    protected function findFiles()
+    {
+        $finder = new Finder;
+
+        return $finder
+            ->files()
+            ->in($this->project)
+            ->notPath('vendor', 'node_modules') // ignore vendor & node_modules folders
+            ->contains($this->reg);
+    }
+
+    protected function outputReferences(?array $references)
+    {
+        echo PHP_EOL;
+        echo PHP_EOL;
+        // Render the output ...
+        if (!empty($references)) {
+            $this->output->writeln("<error>Found trailing dd's!</error>");
+            foreach ($references as $key => $arr) {
+                $this->renderTabulatedData($key, $arr);
+            }
+        } else {
+            $this->output->writeln("<info>No trailing dd's found!</info>");
+        }
+    }
+
+    protected function renderTabulatedData(string $key, array $arr)
+    {
+        $table = new Table($this->output);
+        $this->output->writeln("<--------------------------------------------------------------------");
+        $this->output->writeln('<comment>' . $key . '</comment>');
+        $table->setHeaders(['Line', 'Content'])
+            ->setRows($arr)
+            ->render();
+        $this->output->writeln("-------------------------------------------------------------------->");
+    }
+}
